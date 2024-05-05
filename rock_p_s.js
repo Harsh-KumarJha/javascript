2fad0cd48d311955c6bb1ab8aad0b1db41b5d7da
Harsh-KumarJha <harshbhanujha@gmail.com>
2024-05-05 16:45:00 +0530

    javasript-for-rpsgame

---
+++ rock_p_s.js
@@ -0,0 +1,17 @@
+let computermove='';
+function computerMove(){
+    const randomNumber= Math.random();
+    
+    if(randomNumber>=0&&randomNumber<1/3){
+        computermove='rock';
+    }
+    else if(randomNumber>=1&&randomNumber<2/3){
+        computermove='paper';
+    }
+    else if(randomNumber>=2/3&&randomNumber<1){
+        computermove='scissors';
+    }
+
+    return computermove;
+}
+
