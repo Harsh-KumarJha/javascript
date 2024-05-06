31efee5b48f17e6c10ed62e4ce54d243d2a6b404
Harsh-KumarJha <harshbhanujha@gmail.com>
2024-05-06 13:38:22 +0530

    functions update

--- rock_p_s.js
+++ rock_p_s.js
@@ -1,4 +1,46 @@
-let computermove='';
+
+
+function startGame(playerMove){
+    const computermove=computerMove();
+    let result='';
+    if(playerMove==='rock'){
+        if(computermove==='rock'){
+            result='Tie';
+        }
+        else if(computermove==='paper'){
+            result='You lost.';
+        }
+        else if(computermove==='scissors'){
+            result='You win.';
+        }
+        
+    }
+    else if(playerMove==='paper'){
+        if(computermove==='rock'){
+            result='You Win';
+        }
+        else if(computermove==='paper'){
+            result='Tie.';
+        }
+        else if(computermove==='scissors'){
+            result='You lost.';
+        }
+    }
+    else{
+        if(computermove==='rock'){
+            result='You Lost';
+        }
+        else if(computermove==='paper'){
+            result='You Win.';
+        }
+        else if(computermove==='scissors'){
+            result='Tie.';
+        }
+
+    }
+    alert(`You picked ${playerMove}. Computer picked ${computermove}. ${result}`);
+}
+
 function computerMove(){
     const randomNumber= Math.random();
     
